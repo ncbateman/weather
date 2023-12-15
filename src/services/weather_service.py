94eecfe3a96f3584b3515fc694303f7ba4b3d876
Nick Bateman <ncbateman@gmail.com>
2023-12-15 11:29:17 +0000

    standard unit spacing

--- src/services/weather_service.py
+++ src/services/weather_service.py
@@ -85,7 +85,7 @@ class WeatherService:
         # Extract and format specific weather details from the data point.
         weather_data = {
             'temperature': f"{weather_data_point.get('temp', 'N/A')}C",
-            'pressure': f"{weather_data_point.get('pressure', 'N/A')} hPa",
+            'pressure': f"{weather_data_point.get('pressure', 'N/A')}hPa",
             'humidity': f"{weather_data_point.get('humidity', 'N/A')}%",
             'clouds': f"{weather_data_point.get('clouds', 'N/A')}%"
         }
